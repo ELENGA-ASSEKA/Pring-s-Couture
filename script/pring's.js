9d95895818078fccac8c05ebd5280189c3299cd5
ELENGA-ASSEKA <wilbrechasseka@gmail.com>
2025-03-04 11:45:46 -0800

    push

--- script/pring's.js
+++
@@ -1,4 +0,0 @@
-
-const year = new Date().getFullYear();
-document.getElementById(' currentyear').innerHTML=` &copy; ${year}`;
-document.getElementById(' lastModified').innerHTML=` last Modification: ${document.lastModified}`;
